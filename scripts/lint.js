346ad8fb18eae27f7eda18ca66364385bb6c549f
Steve James <steve@thatdevcompany.com>
2018-05-04 09:10:37 +1000

    Removed IDE from GIT

--- scripts/lint.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var that_build_library_1 = require("that-build-library");
-that_build_library_1.BuildUtils
-    .exec('LINTING', 'tslint', ['-p', 'src/tsconfig.json', '--fix'])
-    .subscribe();
--- scripts/publish.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var that_build_library_1 = require("that-build-library");
-that_build_library_1.BuildUtils
-    .npmPublish('dist', function (pkg) {
-    delete pkg.scripts;
-    pkg.main = 'index.js';
-})
-    .subscribe();
