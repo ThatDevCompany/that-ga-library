3a215ddaeca1d9e77f6c313dd49fde4f3332e3f6
Steve James <steve@thatdevcompany.com>
2018-05-04 09:14:35 +1000

    Removed dist from GIT

--- dist/GACitizen.d.ts
+++
@@ -1,4 +0,0 @@
-export interface GACitizen {
-    mateWith: (other: GACitizen) => GACitizen;
-    mutate: (mutationRate: number) => void;
-}
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './GA';
-export * from './GACitizen';
